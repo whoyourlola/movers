e719f2246f5ddb529701edec17aff85a1924b0c8
Patrick Joseph Puzon <josephpuzon88@gmail.com>
2024-10-10 20:51:17 +0800

    3

--- h3script.js
+++ h3script.js
@@ -1,3 +1,4 @@
+//sidebar dropdown menu
 const allDropdown = document.querySelectorAll('#h3sidebar .h3side-dropdown');
 
 allDropdown.forEach(item=> {
@@ -5,7 +6,37 @@ allDropdown.forEach(item=> {
     a.addEventListener('click', function (e) {
         e.preventDefault();
 
-        this.classList.toggle('h3active');
+        if(!this.classList.contains('h3side-active')){
+            allDropdown.forEach(i=> {
+                const aLink = i.parentElement.querySelector('a:first-child');
+
+                aLink.classList.remove('h3side-active');
+                i.classList.remove('show');
+            })
+        }
+
+        this.classList.toggle('h3side-active');
         item.classList.toggle('show');
     })
+})
+
+
+//profile dropdown menu
+const profile = document.querySelector('nav .h3profile');
+const imgProfile = profile.querySelector('img');
+const dropdownProfile = profile.querySelector('.h3profile-link');
+
+imgProfile.addEventListener('click', function () {
+    dropdownProfile.classList.toggle('show');
+})
+
+
+window.addEventListener('click', function (e) {
+    if (e.target !== imgProfile) {
+        if (e.target !== dropdownProfile){
+            if(dropdownProfile.classList.contains('show')) {
+                dropdownProfile.classList.remove('show');
+            }
+        }
+    }
 })
